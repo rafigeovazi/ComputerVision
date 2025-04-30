f9574de516f2ef959bb67f222f2cf5daaa38ce96
Geosling <145568712+rafigeovazi@users.noreply.github.com>
2025-04-30 10:03:00 +0700

    Add files via upload

---
+++ convert_images.py
@@ -0,0 +1,6 @@
+from face_recognition import convert_images
+
+if __name__ == "__main__":
+    input_dir = "C:/Users/rafig/Documents/PYOpenCV/sigma"
+    output_dir = "C:/Users/rafig/Documents/PYOpenCV/sigma_png"
+    convert_images(input_dir, output_dir)
